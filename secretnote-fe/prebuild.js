b7498be1abe3cb5cf23da81f39c56455a114397f
Markus Bauer <markus.bauer@cispa.saarland>
2020-10-14 22:21:36 +0200

    Use date of last commit instead of current time for build date => more deterministic build

--- secretnote-fe/prebuild.js
+++ secretnote-fe/prebuild.js
@@ -13,18 +13,30 @@ shell.exec("git rev-parse HEAD", {silent: true}, function (error, stdout, stderr
     } else {
         info.git_hash = stdout.trim();
 
-        shell.exec("git describe --abbrev=0 --tags", {silent: true}, function (error, stdout, stderr) {
-            info.version = error ? "" : stdout.trim()
-            console.log(info);
-
-            // Save infos
-            const js = 'export const buildinfo = ' + JSON.stringify(info) + ';';
-            fs.writeFile("src/environments/buildinfo.js", js, function (err) {
-                if (err) {
-                    console.error(err);
-                } else {
-                    console.log("Build information stored");
+        // Get code date from git
+        shell.exec("git log -1 --format=%cd", {silent: true}, function (error, stdout, stderr) {
+            if (!error) {
+                let d = new Date(stdout.trim());
+                if (d) {
+                    info.build_time = d.toLocaleString();
+                    info.build_timestamp = d.getTime();
                 }
+            }
+
+            // Get version from git tag
+            shell.exec("git describe --abbrev=0 --tags", {silent: true}, function (error, stdout, stderr) {
+                info.version = error ? "" : stdout.trim()
+                console.log(info);
+
+                // Save infos
+                const js = 'export const buildinfo = ' + JSON.stringify(info) + ';';
+                fs.writeFile("src/environments/buildinfo.js", js, function (err) {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log("Build information stored");
+                    }
+                });
             });
         });
     }
